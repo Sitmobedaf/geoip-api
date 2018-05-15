074671faf2ec46a15b8b2916369a2c8ebb40a8e9
Kenig <Sitmobedaf@mail.ru>
2018-05-15 22:41:44 +0300

    Update Main class

--- src/eu/theindra/geoip/Main.java
+++ src/eu/theindra/geoip/Main.java
@@ -13,62 +13,70 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.maxmind.geoip.LookupService;
 
-public class Main extends JavaPlugin{
-	
+public class Main extends JavaPlugin {
+
 	public static Main instance;
 	public static LookupService geoip = null;
-	
-	public void onEnable(){		
+
+	public void onEnable() {
 		instance = this;
-		
+
 		Bukkit.getConsoleSender().sendMessage("This API includes GeoLite data created by MaxMind, available from http://www.maxmind.com");
-		
-		if(!new File(getDataFolder(), "GeoLite2-City.mmdb").exists()){
+		File base = new File(getDataFolder(), "GeoLiteCity.dat");
+		boolean mustDownload = false;
+		if (!base.exists()) {
+			mustDownload = true;
+		} else {
+			if (System.currentTimeMillis() - base.lastModified() > 86400000) {
+				mustDownload = true;
+			}
+		}
+		if (mustDownload) {
 			try {
 				downloadFrom("http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz");
-			}catch (IOException e){
+			} catch (IOException e) {
 				Bukkit.getConsoleSender().sendMessage("Something went wrong when downloading the geoip database.");
 				e.printStackTrace();
 				Bukkit.getPluginManager().disablePlugin(this);
 			}
+		} else {
+			Bukkit.getLogger().info("The database was not updated, since the last update was made less than a day ago.");
 		}
-		
 		try {
-			geoip = new LookupService(new File(Main.instance.getDataFolder(), "GeoLiteCity.dat"), LookupService.GEOIP_CHECK_CACHE);
+			geoip = new LookupService(base, LookupService.GEOIP_CHECK_CACHE);
 		} catch (IOException e) {
 			Bukkit.getConsoleSender().sendMessage("Something went wrong.");
 			Bukkit.getPluginManager().disablePlugin(this);
 		}
 	}
-	
-	public void onDisable(){
+
+	public void onDisable() {
 		geoip.close();
 	}
-	
-	public void downloadFrom(String url) throws IOException{
-		URL CityURL = new URL ("http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz");
-		
+
+	public void downloadFrom(String url) throws IOException {
+		URL CityURL = new URL(url);
+
 		URLConnection connection = CityURL.openConnection();
-	    connection.setConnectTimeout(10000);
-	    connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Minecraft server) Bukkit");
-	    connection.connect();
-	    InputStream input = connection.getInputStream();
-	    input = new GZIPInputStream(input);
-	     
-	    // idk why i need this but it throwed an expection
-	    getDataFolder().mkdir();
-	    
-	    OutputStream output = new FileOutputStream(new File(getDataFolder(), "GeoLiteCity.dat"));
+		connection.setConnectTimeout(5000);
+		connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Minecraft server) Bukkit");
+		connection.connect();
+		InputStream input = connection.getInputStream();
+		input = new GZIPInputStream(input);
+
+		// idk why i need this but it throwed an expection
+		getDataFolder().mkdir();
+
+		OutputStream output = new FileOutputStream(new File(getDataFolder(), "GeoLiteCity.dat"));
 		byte[] buffer = new byte[2048];
 		int length = input.read(buffer);
-	    while (length >= 0)
-	    {
+		while (length >= 0) {
 			output.write(buffer, 0, length);
 			length = input.read(buffer);
 		}
-			
-	    // we don't want to leak memory right? 
-	    output.close();
-	    input.close();
+
+		// we don't want to leak memory right?
+		output.close();
+		input.close();
 	}
 }
